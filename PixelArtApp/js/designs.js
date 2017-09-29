dd280995ea9afbe862ded486f6de304e38d1bdf6
mfm92 <mfmoser92@gmail.com>
2017-09-29 23:38:09 +0200

    feat: The user can now create a grid of arbitrary dimensions.
    The user clicks on the single submit button of the page after setting custom values for the height and width of the grid using the two input fields to the left.

--- PixelArtApp/js/designs.js
+++ PixelArtApp/js/designs.js
@@ -2,3 +2,29 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var submitButton = $("input[type='submit']");
+var pixelCanvas = $('#pixel_canvas');
+var inputHeight = $('#input_height');
+var inputWidth = $('#input_width');
+
+var makeGrid = function(evt) {
+  evt.preventDefault();
+  discardGrid();
+
+  const inputHeightVal = inputHeight.val();
+  const inputWidthVal = inputWidth.val();
+
+  for (let i = 0; i < inputWidthVal; i++) {
+    const tableRowElement = $('<tr/>');
+    pixelCanvas.append(tableRowElement);
+
+    for (let j = 0; j < inputHeightVal; j++) {
+      const tableCell = $('<td/>');
+      tableRowElement.append(tableCell);
+    }
+  }
+};
+
+$(function() {
+  submitButton.click(makeGrid);
+});
