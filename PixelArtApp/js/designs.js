4920040d635f6d04e815c64b9e66079740b40d1c
mfm92 <mfmoser92@gmail.com>
2017-09-29 23:50:17 +0200

    docs: Add documentation to each JS function.

--- PixelArtApp/js/designs.js
+++ PixelArtApp/js/designs.js
@@ -3,10 +3,19 @@ var pixelCanvas = $('#pixel_canvas');
 var inputHeight = $('#input_height');
 var inputWidth = $('#input_width');
 
+/**
+* @description Discards the grid by removing the entire subtree
+*   of the pixel canvas in the HTML
+*/
 var discardGrid = function() {
   pixelCanvas.empty();
 }
 
+/**
+* @description Creates a grid of arbitrary dimensions
+*   Also discards the existing one
+* @param {object} evt: The event object
+*/
 var makeGrid = function(evt) {
   evt.preventDefault();
   discardGrid();
@@ -25,6 +34,12 @@ var makeGrid = function(evt) {
   }
 };
 
+/**
+* @description Applies the background-color CSS property
+*   with the colour of the event object to the cell
+*   that the user clicked on.
+* @param {object} evt: The event object
+*/
 var colorTheCell = function(evt) {
   const clickedCell = $(evt.target);
   const colourPicker = $('#colorPicker');
@@ -33,8 +48,12 @@ var colorTheCell = function(evt) {
 };
 
 $(function() {
+  /* setting the number input fields to 20x20 initially
+     makes more sense to me than a default of 1x1 */
   inputWidth.val(20);
   inputHeight.val(20);
+
+  /* apply callbacks */
   submitButton.click(makeGrid);
   pixelCanvas.click('td', colorTheCell);
 });
