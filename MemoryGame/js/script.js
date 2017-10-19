caf4ad478e7c2d54dc9b8a05d7ba3df02096e240
mfm92 <mfmoser92@gmail.com>
2017-10-20 01:48:54 +0200

    feat: Functioning assignment of divs and cards

--- MemoryGame/js/script.js
+++ MemoryGame/js/script.js
@@ -1,26 +1,46 @@
-var numbers = [];
 var secondTryImpending = false;
 
-$(function() {
-  let counter = 0;
-  generateRandomNumbers();
+Array.prototype.shuffle = function() {
+  for (var i = 0; i < this.length; i++) {
+    const randomIndex = Math.floor(Math.random() * this.length);
+    const elementAtIndex = this[randomIndex];
 
-  secondTryImpending ^= secondTryImpending;
-
-  for (element of $('.mainGrid div')) {
-    $(element).text(numbers[counter++]);
+    this[randomIndex] = this[i];
+    this[i] = elementAtIndex;
   }
+}
 
-  $('.mainGrid', 'div').click(function(evt) {
-    if ($(evt.target).is('div')) {
-      $(evt.target).css('background-color', 'white');
-      $(evt.target).toggleClass('card4');
-    }
-  });
+$(function() {
+  setUpImageOrdering();
+
 });
 
-var generateRandomNumbers = function(){
-  for (let i = 0; i < 16; i++) {
-    numbers.push(Math.random());
+var Card = function(nr) {
+  this.classNumber = 'card' + nr;
+};
+
+Card.prototype.alert = function() {
+  alert(this.classNumber);
+}
+
+var setUpImageOrdering = function() {
+  let imageOrdering = [];
+  for (let i = 1; i <= 8; i++) {
+    for (let j = 0; j < 2; j++) {
+      imageOrdering.push(new Card(i));
+    }
+  }
+
+  imageOrdering.shuffle();
+
+  let counter = 0;
+  for (element of $('.mainGrid div')) {
+    const elem = $(element);
+    elem.attr('id', 'field' + counter++);
+    elem.click(function() {
+      const elemId = $(this).attr('id');
+      const id = elemId.substring('field'.length, elemId.length);
+      $(this).toggleClass(imageOrdering[id].classNumber);
+    });
   }
 };
