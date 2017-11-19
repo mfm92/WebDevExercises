a9b97c4b697155a3b9728bf523285a250c773d40
mfm92 <mfmoser92@gmail.com>
2017-11-19 23:50:43 +0100

    chore: comments

--- FeedReaderTesting/jasmine/spec/feedreader.js
+++ FeedReaderTesting/jasmine/spec/feedreader.js
@@ -11,88 +11,91 @@
 "use strict";
 
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
-
-         it('feed urls are defined', function() {
-           for (let feed of allFeeds) {
-             expect(feed.url).toBeDefined();
-           }
-         });
-
-         it('feed names are defined', function() {
-           for (let feed of allFeeds) {
-             expect(feed.name).toBeDefined();
-           }
-         });
-
+  /* This is our first test suite - a test suite just contains
+   * a related set of tests. This suite is all about the RSS
+   * feeds definitions, the allFeeds variable in our application.
+   */
+  describe('RSS Feeds', function() {
+    /* This is our first test - it tests to make sure that the
+     * allFeeds variable has been defined and that it is not
+     * empty. Experiment with this before you get started on
+     * the rest of this project. What happens when you change
+     * allFeeds in app.js to be an empty array and refresh the
+     * page?
+     */
+    it('are defined', function() {
+      expect(allFeeds).toBeDefined();
+      expect(allFeeds.length).not.toBe(0);
     });
 
-    describe('The menu', function() {
-        var menuIcon = $('.menu-icon-link');
-
-        it('menu hidden by default', function() {
-          let bodyHidden = $("body.menu-hidden");
-          expect(bodyHidden.length).not.toBeLessThan(1);
-        });
-
-        it('menu (un)hidden by click', function() {
-          menuIcon.click();
-          let bodyHidden = $("body.menu-hidden");
-          expect(bodyHidden.length).toBeLessThan(1);
-
-          menuIcon.click();
-          bodyHidden = $("body.menu-hidden");
-          expect(bodyHidden.length).not.toBeLessThan(1);
-        });
+    it('feed urls are defined', function() {
+      for (let feed of allFeeds) {
+        expect(feed.url).toBeDefined();
+      }
     });
 
-    describe('Initial entries', function() {
-       beforeEach(done => loadFeed(0, () => done()));
+    it('feed names are defined', function() {
+      for (let feed of allFeeds) {
+        expect(feed.name).toBeDefined();
+      }
+    });
+  });
 
-       it('entry element after loadFeed', function(done) {
-         let entriesInFeedContainer = $(".feed .entry");
-         expect(entriesInFeedContainer.length).not.toBeLessThan(1);
-         done();
-       });
+  describe('The menu', function() {
+    var menuIcon = $('.menu-icon-link');
+
+    it('menu hidden by default', function() {
+      let bodyHidden = $("body.menu-hidden");
+      expect(bodyHidden.length).not.toBeLessThan(1);
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('New feed selection', function() {
-      /* TODO: Write a test that ensures when a new feed is loaded
-       * by the loadFeed function that the content actually changes.
-       * Remember, loadFeed() is asynchronous.
-       */
-       let previousContentFirstLink, afterContentFirstLink;
+    it('menu (un)hidden by click', function() {
+      menuIcon.click();
+      let bodyHidden = $("body.menu-hidden");
+      expect(bodyHidden.length).toBeLessThan(1);
 
-       beforeEach(function(done) {
-         loadFeed(0, function() {
-           previousContentFirstLink = $(".feed .entry-link:first-child").attr('href');
-           loadFeed(2, function() {
-             afterContentFirstLink = $(".feed .entry-link:first-child").attr('href');
-             done();
-           });
-         });
-       });
+      menuIcon.click();
+      bodyHidden = $("body.menu-hidden");
+      expect(bodyHidden.length).not.toBeLessThan(1);
+    });
+  });
 
-       it ('feed changes upon loading', function(done) {
-          console.log("Before: " + previousContentFirstLink + ", after: " + afterContentFirstLink);
-          expect(previousContentFirstLink !== afterContentFirstLink).toBeTruthy();
+  describe('Initial entries', function() {
+    beforeEach(done => loadFeed(0, () => done()));
+
+    it('entry element after loadFeed', function(done) {
+      let entriesInFeedContainer = $(".feed .entry");
+      expect(entriesInFeedContainer.length).not.toBeLessThan(1);
+      done();
+    });
+  });
+
+  describe('New feed selection', function() {
+    let previousContentFirstLink, afterContentFirstLink;
+
+    /*
+     * Load "Udacity blog", take note of the first link of all the loaded entry-links
+     * Load "HTML5 Rocks", do the same.
+     *
+     * Now compare those URLs.
+     * This test assumes that no single URL shows up for more than one "feed name".
+     *
+     * Of course, instead of using these two feed names (Udacity Blog, HTML5 rocks)
+     * one could choose any two of the four available feed names.
+     */
+    beforeEach(function(done) {
+      loadFeed(0, function() {
+        previousContentFirstLink = $(".feed .entry-link:first-child").attr('href');
+        loadFeed(2, function() {
+          afterContentFirstLink = $(".feed .entry-link:first-child").attr('href');
           done();
-       });
+        });
+      });
     });
+
+    it('feed changes upon loading', function(done) {
+      expect(previousContentFirstLink !== afterContentFirstLink).toBeTruthy();
+      done();
+    });
+  });
 }());
